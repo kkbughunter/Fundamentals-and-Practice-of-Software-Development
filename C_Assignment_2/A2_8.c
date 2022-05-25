d5d0a109f62d87620daa9a10e9e60e4c568e01e6
KK BUG HUNTER <91019132+KKBUGHUNTER@users.noreply.github.com>
2022-05-25 10:51:44 +0530

    Update A2_8.c

--- C_Assignment_2/A2_8.c
+++ C_Assignment_2/A2_8.c
@@ -4,21 +4,6 @@
 int main()
 {
     int num;
-    do{
-        printf("Enter the Number : \b");
-        scanf("%d",&num);
-        if (num>0 && num<2001){
-            if (num % 2 == 0){
-                printf("%d is even number.\n",num);
-            }
-            else{
-                printf("%d is odd number.\n",num);
-            }
-        }
-        else{
-            printf("Enter the Valid Number.\n");
-            num=0;
-        }
-    }while( num==0 );
+    Print("
     return 0;
 }
