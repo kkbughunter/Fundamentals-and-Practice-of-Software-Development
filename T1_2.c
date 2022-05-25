ff608dc9c168e5645faef5856576eb66aa993f1d
KK BUG HUNTER <91019132+KKBUGHUNTER@users.noreply.github.com>
2022-05-25 12:50:02 +0530

    Create T1_2.c

---
+++ T1_2.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+int main()
+{
+	printf("A for Addition\nS for ubraction\nM for Multiolication\nD for Division \n       q for exit\n\n");
+	char opt;
+	float a, b;
+	while (1){
+		printf("Enter the option : \b");scanf(" %c",&opt);
+		if (opt == 'q')break;
+		if (opt == 'a' || opt == 's' || opt == 'm' || opt == 'd' ){
+			printf("Enter the Value for A : \b");scanf("%f",&a);
+			printf("Enter the Value for B : \b");scanf("%f",&b);
+			switch(opt){
+				case('a'):{
+					printf("%.2f + %.2f = %.2f\n\n",a,b,a+b);
+					break;
+				}
+				case('s'):{
+					printf("%.2f - %.2f = %.2f\n\n",a,b,a-b);
+					break;
+				}
+				case('m'):{
+					printf("%.2f x %.2f = %.2f\n\n",a,b,a*b);
+					break;
+				}
+				case('d'):{
+					if (b==0){
+						printf("\tThis function is INFINITIVE\n");
+					}
+					else{
+						printf("%.2f / %.2f = %.2f\n\n",a,b,a/b);
+					}
+					break;
+				}
+				default:{
+					printf("Enter the correct opt.\n\n");
+					break;
+				}
+			}
+		}
+		else{
+			printf("\nEnter the valide Input\n\n");
+		}
+	}
+	return 0;
+}
