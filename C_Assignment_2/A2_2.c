b7177c67669b3b33070220409339c2d420147d4e
KK BUG HUNTER <91019132+KKBUGHUNTER@users.noreply.github.com>
2022-05-25 17:36:15 +0000

    Update A2_2.c

--- C_Assignment_2/A2_2.c
+++ C_Assignment_2/A2_2.c
@@ -4,21 +4,21 @@ int main()
 {
     float inputval;
     int opt;
-    printf("1. Find Calsus to Fahrenheit\n2. Find Calsus to Kelvan\n3. Find Fahrenheit to Calsus\nEnter your choice : \b");
+    printf("1. Find Celsius to Fahrenheit\n2. Find Celsius to Kelvin\n3. Find Fahrenheit to Celsius\nEnter your choice : \b");
     scanf("%d",&opt);
     printf("Enter the value : \b");
     scanf("%f",&inputval);
     switch (opt){
         case 1:{
-            printf("Calsus to Fahrenheit : %f\n\n",(inputval* 9/5)+32);
+            printf("Celsius to Fahrenheit : %f\n\n",(inputval* 9/5)+32);
             break;
         }
         case 2:{
-            printf("Calsus to Kelvan : %f\n\n", inputval + 273.15);
+            printf("Celsius to Kelvin : %f\n\n", inputval + 273.15);
             break;
         }
         case 3:{
-            printf("Fahrenheit to Calsus : %f\n\n",(inputval-32)*5/9);
+            printf("Fahrenheit to Celsius : %f\n\n",(inputval-32)*5/9);
             break;
         }
         default:{
