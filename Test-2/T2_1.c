e7299204d865962343a3d841c9fd53b77f595031
KK BUG HUNTER <91019132+KKBUGHUNTER@users.noreply.github.com>
2022-06-19 14:44:47 +0000

    Update T2_1.c

--- Test-2/T2_1.c
+++ Test-2/T2_1.c
@@ -15,22 +15,11 @@ int fun(int num){
 
 int main()
 {
-	int num=0;
-	printf("Enter the number : ");scanf("%d",&num);
-	num = fun(num);
-	printf("%d",num);
-	return 0;
-}
-
-int main()
-{
     int n,n1;
     printf("Enter the numbers : ");scanf("%d",&n);
-    
-    n1 = fun(n);
-    printf("\nSum of digits : %d\n\n",n1);
-    
-    n1 = fun(n1);
-    printf("\nSum of digits of number : %d\n\n",n1);
+    while(n>9){
+		n = fun(n);
+		printf("\nSum of digits : %d\n\n",n);
+	}
     return 0;
 }
