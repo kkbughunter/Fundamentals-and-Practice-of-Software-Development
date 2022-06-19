a2cc3d68d73708be1454008b62b75874aa26d91a
KK BUG HUNTER <91019132+KKBUGHUNTER@users.noreply.github.com>
2022-06-19 14:41:40 +0000

    Update T2_1.c

--- Test-2/T2_1.c
+++ Test-2/T2_1.c
@@ -1,39 +1,36 @@
-// question 1
 #include<stdio.h>
-int Add_Dig(int num);
-int Add_Ans_Dig(int n);
+int fun(int num);
 
-int Add_Dig(int num)
-{
-    int sum = 0;
-    while(num>0){
-        sum = sum + (num % 10 );
-        num = num / 10;
-    }
-    return sum;
+int fun(int num){
+	int n=0,sum=0;
+	if (num == 0)
+		return 0;
+	else{
+		n = num %10;
+		return sum = n + fun(num/10);
+	}
+	
 }
 
 
-int Add_Ans_Dig(int n)
+int main()
 {
-    int sum1 = 0;
-    while(n>0){
-        sum1 = sum1 + (n % 10);
-        n = n / 10;
-    }
-    return sum1;
+	int num=0;
+	printf("Enter the number : ");scanf("%d",&num);
+	num = fun(num);
+	printf("%d",num);
+	return 0;
 }
-
 
 int main()
 {
     int n,n1;
     printf("Enter the numbers : ");scanf("%d",&n);
     
-    n1 = Add_Dig(n);
+    n1 = fun(n);
     printf("\nSum of digits : %d\n\n",n1);
     
-    n1 = Add_Ans_Dig(n1);
+    n1 = fun(n1);
     printf("\nSum of digits of number : %d\n\n",n1);
     return 0;
 }
