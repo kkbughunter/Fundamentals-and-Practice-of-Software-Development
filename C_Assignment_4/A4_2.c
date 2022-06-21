7dc7d6937feb5c8fbf322a6917cf739732b117f4
KK BUG HUNTER <91019132+KKBUGHUNTER@users.noreply.github.com>
2022-06-21 15:16:26 +0000

    Add files via upload

---
+++ C_Assignment_4/A4_2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+void pushZerosToEnd(int arr[], int n);
+
+void pushZerosToEnd(int arr[], int n)
+{
+	int move = 0;
+	for (int i = 0; i < n; i++)
+		if (arr[i] != 0)
+			arr[move++] = arr[i];
+	while (move < n)
+		arr[move++] = 0;
+}
+
+int main()
+{
+	int arr[] = {5, 3, 0, 1, 3, 0, 8, 0};
+	int n = sizeof(arr) /4;
+	pushZerosToEnd(arr, n);
+	for (int i = 0; i < n; i++)
+	printf("%d,", arr[i]);
+	return 0;
+}
