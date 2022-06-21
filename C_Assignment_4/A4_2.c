a3250ab41860d10345871326db3a1d44103efa25
KK BUG HUNTER <91019132+KKBUGHUNTER@users.noreply.github.com>
2022-06-21 15:18:54 +0000

    Update A4_2.c

--- C_Assignment_4/A4_2.c
+++ C_Assignment_4/A4_2.c
@@ -4,11 +4,14 @@ void pushZerosToEnd(int arr[], int n);
 void pushZerosToEnd(int arr[], int n)
 {
 	int move = 0;
-	for (int i = 0; i < n; i++)
-		if (arr[i] != 0)
+	for (int i = 0; i < n; i++){
+		if (arr[i] != 0){
 			arr[move++] = arr[i];
-	while (move < n)
+            }
+        }
+	while (move < n){
 		arr[move++] = 0;
+    }
 }
 
 int main()
