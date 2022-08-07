240461135ba283d189d817fbcabb0ad67d9d2e8f
Karthikeyan_A <91019132+KKBUGHUNTER@users.noreply.github.com>
2022-08-06 22:55:20 -0700

    Delete C_Assignment_2 directory

--- C_Assignment_2/A2_3.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main()
-{
-    int num;
-    printf("Enter the Number : \b");
-    scanf("%d",&num);
-    (num % 2 == 0) ? printf("%d is EVEN Number.\n",num) : printf("%d is ODD Number.\n",num);
-    return 0;
-}
