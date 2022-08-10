c10d4c27a17a160c6b2450b0f3411ae32cada320
Karthikeyan_A <91019132+KKBUGHUNTER@users.noreply.github.com>
2022-08-09 21:49:07 -0700

    Update string_computer.c

--- Mini Project 1/string_computer.c
+++ Mini Project 1/string_computer.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+void String_Computer(char str1[], char str2[]);
 char str1[100], str2[100];
 int dp[100][100], arg[100][100];
 void display(int i, int j) {
@@ -22,29 +23,39 @@ void display(int i, int j) {
 	    }
 
 }
-int main() {
-    int i, j;
-    char output[20][40];
-    printf("Enter the String 1  2 : ");
-    while(scanf("%s %s", str1, str2) == 2) {
-        memset(dp, 63, sizeof(dp));
+
+
+
+void String_Computer(char str1[], char str2[])
+{
+	memset(dp, 63, sizeof(dp));
         int len_str1 = strlen(str1), len_str2 = strlen(str2);
         dp[0][0] = 0;
+        int i,j;
         for(i = 0; i <= len_str1; i++) {
             for(j = 0; j <= len_str2; j++) {
-                if(str1[i] == str2[j] && dp[i+1][j+1] > dp[i][j]) // copy
+//            	printf("%d\n",dp[i][j]);
+                if(str1[i] == str2[j] && dp[i+1][j+1] > dp[i][j])
                     dp[i+1][j+1] = dp[i][j], arg[i+1][j+1] = 1;
-                if(dp[i+1][j] > dp[i][j]+1) // delete
+                if(dp[i+1][j] > dp[i][j]+1) 
                     dp[i+1][j] = dp[i][j]+1, arg[i+1][j] = 2;
-                if(dp[i][j+1] > dp[i][j]+1) // insert
+                if(dp[i][j+1] > dp[i][j]+1) 
                     dp[i][j+1] = dp[i][j]+1, arg[i][j+1] = 3;
-                if(dp[i+1][j+1] > dp[i][j]+1) // change
+                if(dp[i+1][j+1] > dp[i][j]+1) 
                     dp[i+1][j+1] = dp[i][j]+1, arg[i+1][j+1] = 4;
             }
         }
         printf("The Output is : ");
         display(len_str1, len_str2);
         printf("E\n\n");
+}
+
+int main() {
+    int i, j;
+    char output[20][40];
+    printf("Enter the String 1  2 : ");
+    while(scanf("%s %s", str1, str2) == 2) {
+        String_Computer(str1,str2);
     }
     return 0;
 }
